ad807497a9857c73a1a4ea6c5275e1d19fceee3c
Justin Douville <jdouville8@gmail.com>
2021-03-29 12:59:38 -0400

    added file adding routes, have not yet got them functioning

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/navBar/NavBar.js";
+import FileSubmit from "./pages/fileSubmit"
+
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <FileSubmit />
     </div>
   );
 }
