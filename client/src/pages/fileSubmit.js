f995acf0076c81a4fe2d4de50c6ba0564b5a2a57
Justin Douville <jdouville8@gmail.com>
2021-03-29 21:03:11 -0400

    merge from main

--- client/src/pages/fileSubmit.js
+++ client/src/pages/fileSubmit.js
@@ -64,7 +64,7 @@ export default function FileSubmit() {
 							variant="contained"
 							color="primary"
 							value="Upload"
-							onClick={log}>
+							onClick={onSubmit}>
 							Submit
 						</Button>
 					</form>
