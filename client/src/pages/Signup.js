08dab40b101de329368a69c78afa1f79bb019595
seff0 <76668281+seff0@users.noreply.github.com>
2021-03-31 15:38:10 -0600

    styling on login and signup pages

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,100 +1,88 @@
 import React from "react";
 import {
-  Grid,
-  Card,
-  CardActions,
-  CardContent,
-  Button,
-  TextField,
-  makeStyles,
+	Grid,
+	Card,
+	CardActions,
+	CardContent,
+	Button,
+	TextField,
+	makeStyles,
 } from "@material-ui/core";
 import CoolImg from "../images/landing.png";
 import Background from "../images/wallpaper.jpg";
 
 const useStyles = makeStyles({
-  root: {
-    minWidth: 350,
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
+	root: {
+		minWidth: 350,
+	},
+	title: {
+		fontSize: 14,
+	},
+	pos: {
+		marginBottom: 12,
+	},
 });
 
 function Signup() {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  const onSubmit = () => {};
+	const onSubmit = () => {};
 
-  return (
-    <div
-      style={{
-        height: "100vh",
-        width: "100%",
-      }}
-    >
-      <Grid>
-        <Grid item lg={7}>
-          <div
-            style={{
-              position: "absolute",
-              left: "32%",
-              top: "50%",
-              transform: "translate(-50%, -50%)",
-            }}
-          >
-            <img src={CoolImg} />
-          </div>
-        </Grid>
-        <Grid item lg={5}>
-          <div
-            style={{
-              position: "absolute",
-              left: "77%",
-              top: "50%",
-              transform: "translate(-50%, -50%)",
-            }}
-          >
-            <Card className={classes.root} variant="outlined">
-              <form>
-                <CardActions>
-                  <TextField
-                    id="outlined-basic"
-                    label="Email"
-                    variant="outlined"
-                    fullWidth
-                  />
-                </CardActions>
-                <CardActions>
-                  <TextField
-                    id="outlined-basic"
-                    label="Username"
-                    variant="outlined"
-                    fullWidth
-                  />
-                </CardActions>
-                <CardActions>
-                  <TextField
-                    id="outlined-basic"
-                    label="Password"
-                    variant="outlined"
-                    fullWidth
-                  />
-                </CardActions>
-                <CardActions>
-                  <Button size="medium" onClick={onSubmit}>
-                    Signup
-                  </Button>
-                </CardActions>
-              </form>
-            </Card>
-          </div>
-        </Grid>
-      </Grid>
-    </div>
-  );
+	return (
+		<div
+			style={{
+				height: "100vh",
+				width: "100%",
+			}}>
+			<Grid>
+				<Grid item lg={7}>
+					<div
+						style={{
+							position: "absolute",
+							left: "32%",
+							top: "50%",
+							transform: "translate(-50%, -50%)",
+						}}>
+						<img src={CoolImg} />
+					</div>
+				</Grid>
+				<Grid item lg={5}>
+					<div
+						style={{
+							position: "absolute",
+							left: "77%",
+							top: "50%",
+							transform: "translate(-50%, -50%)",
+							opacity: ".95",
+							boxShadow: "5px 5px 15px grey",
+						}}>
+						<Card className={classes.root} variant="outlined">
+							<form>
+								<CardActions>
+									<TextField id="outlined-basic" label="Email" fullWidth />
+								</CardActions>
+								<CardActions>
+									<TextField id="outlined-basic" label="Username" fullWidth />
+								</CardActions>
+								<CardActions>
+									<TextField id="outlined-basic" label="Password" fullWidth />
+								</CardActions>
+								<CardActions>
+									<Button
+										color="secondary"
+										variant="outlined"
+										size="medium"
+										onClick={onSubmit}>
+										Signup
+									</Button>
+								</CardActions>
+							</form>
+						</Card>
+					</div>
+				</Grid>
+			</Grid>
+		</div>
+	);
 }
 
 export default Signup;
