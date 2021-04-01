d17dca38bb634c61ae15ec6812015d46f1780e2b
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-04-01 11:04:29 -0600

    add auth0 to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Auth0ProviderWithHistory from './auth/auth0-provider-with-history';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+	<Auth0ProviderWithHistory>
+		<App />
+	</Auth0ProviderWithHistory>,
+
+	document.getElementById('root')
+);
 registerServiceWorker();
