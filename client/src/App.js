faced3190a94d7ab8bb3ca3ded04cbdd45884b50
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-04-01 12:08:35 -0600

    initial auth0 setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,57 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Search from "./pages/Search";
-import NavBar from "./components/navBar/NavBar.js";
-import Landing from "./pages/Landing";
-import FileSubmit from "./pages/fileSubmit";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Home from "./pages/Home";
-import Footer from "../src/components/Footer/Footer";
-import Background from "./images/wallpaper.jpg";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import Auth0ProviderWithHistory from './auth/auth0-provider-with-history';
+import Search from './pages/Search';
+import NavBar from './components/navBar/NavBar.js';
+import Landing from './pages/Landing';
+import FileSubmit from './pages/fileSubmit';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+import Footer from '../src/components/Footer/Footer';
+import Background from './images/wallpaper.jpg';
 
 function App() {
-  return (
-    <Router>
-      <div
-        style={{
-          backgroundImage: `url(${Background})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          backgroundPosition: "center center",
-          backgroundAttachment: "fixed",
-        }}
-      >
-        <NavBar />
-        <Switch>
-          <Route exact path={"/search"}>
-            <Search />
-          </Route>
-          <Route exact path={["/", "/landing"]}>
-            <Landing />
-          </Route>
-          <Route exact path={"/upload"}>
-            <FileSubmit />
-          </Route>
-          <Route exact path={"/login"}>
-            <Login />
-          </Route>
-          <Route exact path={"/signup"}>
-            <Signup />
-          </Route>
-          <Route exact path={"/home"}>
-            <Home />
-          </Route>
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			{/* <Auth0ProviderWithHistory> */}
+			<div
+				style={{
+					backgroundImage: `url(${Background})`,
+					backgroundRepeat: 'no-repeat',
+					backgroundSize: 'cover',
+					backgroundPosition: 'center center',
+					backgroundAttachment: 'fixed',
+				}}
+			>
+				<NavBar />
+				<Switch>
+					<Route exact path={'/search'}>
+						<Search />
+					</Route>
+					<Route exact path={['/', '/landing']}>
+						<Landing />
+					</Route>
+					<Route exact path={'/upload'}>
+						<FileSubmit />
+					</Route>
+					<Route exact path={'/login'}>
+						<Login />
+					</Route>
+					<Route exact path={'/signup'}>
+						<Signup />
+					</Route>
+					<Route exact path={'/home'}>
+						<Home />
+					</Route>
+				</Switch>
+				<Footer />
+			</div>
+			{/* </Auth0ProviderWithHistory> */}
+		</Router>
+	);
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Auth0ProviderWithHistory from './auth/auth0-provider-with-history';
+import { Auth0Provider } from '@auth0/auth0-react';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-	<Auth0ProviderWithHistory>
-		<App />
-	</Auth0ProviderWithHistory>,
-
+	<Router>
+		<Auth0Provider
+			domain="wavmovers.us.auth0.com"
+			clientId="IEGWrp4HIHw4dv4dcMoWlpgdpGA1IVrV"
+			redirectUri={window.location.origin}
+		>
+			{/* <Auth0ProviderWithHistory> */}
+			<App />
+			{/* </Auth0ProviderWithHistory> */}
+		</Auth0Provider>
+	</Router>,
 	document.getElementById('root')
 );
 registerServiceWorker();
