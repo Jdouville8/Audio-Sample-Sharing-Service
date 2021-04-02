794ad9d2b395b97ad2395c9f9fc81f0bb8a04ec6
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-04-02 14:45:55 -0600

    create PlayerContext.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Search from "./pages/Search";
-import NavBar from "./components/navBar/NavBar.js";
-import Landing from "./pages/Landing";
-import FileSubmit from "./pages/fileSubmit";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Home from "./pages/Home";
-import Education from "./pages/EducationalResources";
-import Footer from "./components/Footer/Footer";
-import UserProfile from "./pages/userProfile";
-import Background from "./images/wallpaper.jpg";
-import NoMatch from "./pages/NoMatch";
-import Lessons from "./pages/Lessons";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PlayerContext from './utils/PlayerContext';
+import Search from './pages/Search';
+import NavBar from './components/navBar/NavBar.js';
+import Landing from './pages/Landing';
+import FileSubmit from './pages/fileSubmit';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+import Education from './pages/EducationalResources';
+import Footer from './components/Footer/Footer';
+import UserProfile from './pages/userProfile';
+import Background from './images/wallpaper.jpg';
+import NoMatch from './pages/NoMatch';
+import Lessons from './pages/Lessons';
 
 function App() {
 	return (
@@ -21,35 +22,36 @@ function App() {
 			<div
 				style={{
 					backgroundImage: `url(${Background})`,
-					backgroundRepeat: "no-repeat",
-					backgroundSize: "cover",
-					backgroundPosition: "center center",
-					backgroundAttachment: "fixed",
-				}}>
+					backgroundRepeat: 'no-repeat',
+					backgroundSize: 'cover',
+					backgroundPosition: 'center center',
+					backgroundAttachment: 'fixed',
+				}}
+			>
 				<NavBar />
 				<Switch>
-					<Route exact path={"/search"}>
+					<Route exact path={'/search'}>
 						<Search />
 					</Route>
-					<Route exact path={["/", "/landing"]}>
+					<Route exact path={['/', '/landing']}>
 						<Landing />
 					</Route>
-					<Route exact path={"/upload"}>
+					<Route exact path={'/upload'}>
 						<FileSubmit />
 					</Route>
-					<Route exact path={"/login"}>
+					<Route exact path={'/login'}>
 						<Login />
 					</Route>
-					<Route exact path={"/signup"}>
+					<Route exact path={'/signup'}>
 						<Signup />
 					</Route>
-					<Route exact path={"/home"}>
+					<Route exact path={'/home'}>
 						<Home />
 					</Route>
-					<Route exact path={"/lessons"}>
+					<Route exact path={'/lessons'}>
 						<Lessons />
 					</Route>
-					<Route exact path={"/education"}>
+					<Route exact path={'/education'}>
 						<Education />
 					</Route>
 					<Route>
---
+++ client/src/components/utils/PlayerContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PlayerContext = React.createContext({
+  audioSrc = ""
+});
+
+export default PlayerContext;
