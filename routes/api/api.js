bb3232e435b159a6e301bdc7e6ffefe456d99946
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-04-02 11:51:28 -0600

    replace mongo env variable

--- routes/api/api.js
+++ routes/api/api.js
@@ -18,7 +18,7 @@ db.once('open', function () {
 });
 
 const storage = new GridFsStorage({
-	url: 'mongodb://localhost/SampleLibrary',
+	url: process.env.MONGODB_URI,
 	file: (req, file) => {
 		return new Promise((resolve, reject) => {
 			crypto.randomBytes(16, (err, buf) => {
