acd465426e9c980cbef4abd238b1f6996776c7f9
Justin Douville <jdouville8@gmail.com>
2021-04-02 12:31:53 -0400

    merging from main

--- server.js
+++ server.js
@@ -38,10 +38,10 @@ app.use(function (req, res, next) {
 // Serve static files from the React app
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
-	app.get('/*', function (req, res) {
-		res.sendFile(path.join(__dirname, 'build', 'index.html'));
-	  });
-	app.use(express.static(path.join(__dirname, 'build')));
+	// app.get('/*', function (req, res) {
+	// 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+	//   });
+	//   app.use(express.static(path.join(__dirname, 'build')));
 }
 
 app.use(express.json());
