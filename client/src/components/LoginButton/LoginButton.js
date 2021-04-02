e1bb3e13e4d4bbf3c070bfd1c5ecdf9d918eaaa8
seff0 <76668281+seff0@users.noreply.github.com>
2021-04-02 14:28:49 -0600

    add pointer functionality to login button

--- client/src/components/LoginButton/LoginButton.js
+++ client/src/components/LoginButton/LoginButton.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Link, Tooltip } from "@material-ui/core";
-
+import { Link, Tooltip, makeStyles } from "@material-ui/core";
 import AccountBox from "@material-ui/icons/AccountBox";
 
+const useStyles = makeStyles((theme) => ({
+	login: {
+		cursor: "pointer",
+	},
+}));
+
 function LoginButton() {
+	const classes = useStyles();
 	const { loginWithRedirect } = useAuth0();
 
 	return (
 		<Tooltip title="Login">
 			<Link
+				className={classes.login}
 				style={{ color: "pink", margin: "5px", marginTop: "9%" }}
 				onClick={() => loginWithRedirect()}>
 				<AccountBox />
