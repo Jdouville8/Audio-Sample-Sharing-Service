9fd6ad29463efa7ed5fa43694c7d9025e4703764
Perkyderm <ncgoedecke@gmail.com>
2021-04-02 12:41:35 -0600

    images

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
 import "./App.css";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import { useAuth0 } from "@auth0/auth0-react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Search from "./pages/Search";
 import NavBar from "./components/navBar/NavBar.js";
 import Landing from "./pages/Landing";
@@ -10,9 +8,11 @@ import FileSubmit from "./pages/fileSubmit";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
-import Footer from "../src/components/Footer/Footer";
+import Footer from "./components/Footer/Footer";
+import UserProfile from "./pages/userProfile";
 import Background from "./images/wallpaper.jpg";
 import NoMatch from "./pages/NoMatch";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -46,6 +46,9 @@ function App() {
           <Route exact path={"/home"}>
             <Home />
           </Route>
+          <Route exact path={"/about"}>
+            <About />
+          </Route>
           <Route>
             <NoMatch />
           </Route>
@@ -54,63 +57,6 @@ function App() {
       </div>
     </Router>
   );
-=======
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Search from './pages/Search';
-import NavBar from './components/navBar/NavBar.js';
-import Landing from './pages/Landing';
-import FileSubmit from './pages/fileSubmit';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Home from './pages/Home';
-import Footer from './components/Footer/Footer';
-import UserProfile from './pages/userProfile';
-import Background from './images/wallpaper.jpg';
-import NoMatch from './pages/NoMatch';
-
-function App() {
-	return (
-		<Router>
-			<div
-				style={{
-					backgroundImage: `url(${Background})`,
-					backgroundRepeat: 'no-repeat',
-					backgroundSize: 'cover',
-					backgroundPosition: 'center center',
-					backgroundAttachment: 'fixed',
-				}}
-			>
-				<NavBar />
-				<Switch>
-					<Route exact path={'/search'}>
-						<Search />
-					</Route>
-					<Route exact path={['/', '/landing']}>
-						<Landing />
-					</Route>
-					<Route exact path={'/upload'}>
-						<FileSubmit />
-					</Route>
-					<Route exact path={'/login'}>
-						<Login />
-					</Route>
-					<Route exact path={'/signup'}>
-						<Signup />
-					</Route>
-					<Route exact path={'/home'}>
-						<Home />
-					</Route>
-					<Route>
-						<NoMatch />
-					</Route>
-				</Switch>
-				<Footer />
-			</div>
-		</Router>
-	);
->>>>>>> main
 }
 
 export default App;
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,19 +1,54 @@
 import React from "react";
-import Devs from "../dev.json";
-import DevCard from "../components/DevCard";
+
+import DevCard from "../components/DevCard/DevCard";
+import Justin from "../images/Justin.jpeg";
+import Nick from "../images/Nick.jpg";
+import Luke from "../images/Luke.jpg";
+import Seth from "../images/Seth.jpg";
 
 function About() {
-  const info = Devs;
+  const devs = [
+    {
+      name: "Justin Douville",
+      about: "Blurbbbbb",
+      gitHub: "https://github.com/Jdouville8",
+      pic: `${Justin}`,
+      role: "Back-End Dev",
+    },
+    {
+      name: "Luke Oxner",
+      about: "Blurbbbbb",
+      gitHub: "https://github.com/lukeoxner",
+      pic: `${Luke}`,
+      role: "Back-End Dev",
+    },
+    {
+      name: "Seth Hill",
+      about: "Blurbbbbb",
+      gitHub: "https://github.com/seff0",
+      pic: `${Seth}`,
+      role: "Front-End Dev",
+    },
+    {
+      name: "Nick Goedecke",
+      about: "Blurbbbbb",
+      gitHub: "https://github.com/Perkyderm",
+      pic: `${Nick}`,
+      role: "Front-End Dev",
+    },
+  ];
 
   return (
-    <div>
-      <DevCard
-        pic={info.pic}
-        name={info.name}
-        role={info.role}
-        about={info.about}
-        gitHub={info.gitHub}
-      />
+    <div style={{ height: "100vh" }}>
+      {devs.map((dev) => (
+        <DevCard
+          pic={dev.pic}
+          name={dev.name}
+          role={dev.role}
+          about={dev.about}
+          gitHub={dev.gitHub}
+        />
+      ))}
     </div>
   );
 }
