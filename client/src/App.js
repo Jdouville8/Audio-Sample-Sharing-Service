67fda6581a06c44b8ec14724c47a67868a297f85
seff0 <76668281+seff0@users.noreply.github.com>
2021-04-02 11:55:43 -0600

    implemented calendly api

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Search from './pages/Search';
-import NavBar from './components/navBar/NavBar.js';
-import Landing from './pages/Landing';
-import FileSubmit from './pages/fileSubmit';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Home from './pages/Home';
-import Footer from './components/Footer/Footer';
-import UserProfile from './pages/userProfile';
-import Background from './images/wallpaper.jpg';
-import NoMatch from './pages/NoMatch';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Search from "./pages/Search";
+import NavBar from "./components/navBar/NavBar.js";
+import Landing from "./pages/Landing";
+import FileSubmit from "./pages/fileSubmit";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import Footer from "./components/Footer/Footer";
+import UserProfile from "./pages/userProfile";
+import Background from "./images/wallpaper.jpg";
+import NoMatch from "./pages/NoMatch";
+import Lessons from "./pages/Lessons";
 
 function App() {
 	return (
@@ -19,31 +20,33 @@ function App() {
 			<div
 				style={{
 					backgroundImage: `url(${Background})`,
-					backgroundRepeat: 'no-repeat',
-					backgroundSize: 'cover',
-					backgroundPosition: 'center center',
-					backgroundAttachment: 'fixed',
-				}}
-			>
+					backgroundRepeat: "no-repeat",
+					backgroundSize: "cover",
+					backgroundPosition: "center center",
+					backgroundAttachment: "fixed",
+				}}>
 				<NavBar />
 				<Switch>
-					<Route exact path={'/search'}>
+					<Route exact path={"/search"}>
 						<Search />
 					</Route>
-					<Route exact path={['/', '/landing']}>
+					<Route exact path={["/", "/landing"]}>
 						<Landing />
 					</Route>
-					<Route exact path={'/upload'}>
+					<Route exact path={"/upload"}>
 						<FileSubmit />
 					</Route>
-					<Route exact path={'/login'}>
+					<Route exact path={"/login"}>
 						<Login />
 					</Route>
-					<Route exact path={'/signup'}>
+					<Route exact path={"/signup"}>
 						<Signup />
 					</Route>
-					<Route exact path={'/home'}>
+					<Route exact path={"/home"}>
 						<Home />
+					</Route>
+					<Route exact path={"/lessons"}>
+						<Lessons />
 					</Route>
 					<Route>
 						<NoMatch />
--- client/src/pages/Lessons.js
+++ client/src/pages/Lessons.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { InlineWidget } from "react-calendly";
 
 function Lessons() {
-  return <div></div>;
+	return (
+		<div
+			style={{
+				height: "100vh",
+				width: "100%",
+			}}>
+			<InlineWidget url="https://calendly.com/seff0" />
+		</div>
+	);
 }
 
 export default Lessons;
