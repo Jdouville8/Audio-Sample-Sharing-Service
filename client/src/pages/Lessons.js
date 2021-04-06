cd3a73d60ecd3f81f214bb6665420ff943008aa0
Perkyderm <ncgoedecke@gmail.com>
2021-04-06 00:14:51 -0600

    marginsssssssssssss

--- client/src/pages/Lessons.js
+++ client/src/pages/Lessons.js
@@ -1,101 +1,101 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-	Card,
-	Typography,
-	CardContent,
-	makeStyles,
-	Button,
-} from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
-import { InlineWidget } from 'react-calendly';
-import CheckoutContainer from '../components/CheckoutContainer/index';
-import { useAuth0 } from '@auth0/auth0-react';
-import Login from './Login';
+  Card,
+  Typography,
+  CardContent,
+  makeStyles,
+  Button,
+} from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import { InlineWidget } from "react-calendly";
+import CheckoutContainer from "../components/CheckoutContainer/index";
+import { useAuth0 } from "@auth0/auth0-react";
+import Login from "./Login";
 
 const useStyles = makeStyles({
-	root: {
-		marginTop: '19%',
-	},
-	cardTitle: {
-		fontWeight: '20px',
-	},
-	button: {
-		color: '#5e235b',
-		margin: 'auto',
-		backgroundColor: '#8989fc',
-	},
+  root: {
+    marginTop: "8%",
+  },
+  cardTitle: {
+    fontWeight: "20px",
+  },
+  button: {
+    color: "#5e235b",
+    margin: "auto",
+    backgroundColor: "#8989fc",
+  },
 });
 
 function Lessons() {
-	const { isAuthenticated } = useAuth0();
-	const classes = useStyles();
+  const { isAuthenticated } = useAuth0();
+  const classes = useStyles();
 
-	const [showItem, setShowItem] = useState(false);
+  const [showItem, setShowItem] = useState(false);
 
-	if (isAuthenticated) {
-		return (
-			<div
-				style={{
-					height: '120vh',
-					width: '100%',
-				}}
-			>
-				<Grid container direction="row" alignItems="center" justify="center">
-					<Grid
-						item
-						style={{ height: '700px', width: '60%', minWidth: '500px' }}
-					>
-						<Card
-							className={classes.root}
-							variant="outlined"
-							style={{
-								backgroundColor: `rgba(0,0,0,.7)`,
-								color: 'white',
-								boxShadow: '5px 5px 15px #424242',
-							}}
-						>
-							<CardContent>
-								<Typography
-									style={{ opacity: '1' }}
-									className={classes.cardTitle}
-								>
-									Are you interested in receiving technical and industry related
-									feedback from your favorite artists in the scene?
-								</Typography>
-								<br />
-								<Typography>
-									<b>WavMovers</b> offers 1-on-1 lesson scheduling with
-									professional artists to give you the tools you need to create.
-									Our artists can cover a vast array of topics including: Track
-									arrangement, DAW Workflow, Sound design, Mixing, Mastering,
-									DJing, Music theory, Track feedback, Industry consulting, and
-									Artistic identity guidance
-								</Typography>
-								<br />
-								<Typography>
-									WavMovers 1-on-1 lessons will empower you to grow leaps and
-									bounds in your musical journey through honing the skills
-									needed to get you from the bedroom to the main stage
-								</Typography>
-								<br />
+  if (isAuthenticated) {
+    return (
+      <div
+        style={{
+          height: "120vh",
+          width: "100%",
+        }}
+      >
+        <Grid container direction="row" alignItems="center" justify="center">
+          <Grid
+            item
+            style={{ height: "700px", width: "60%", minWidth: "500px" }}
+          >
+            <Card
+              className={classes.root}
+              variant="outlined"
+              style={{
+                backgroundColor: `rgba(0,0,0,.7)`,
+                color: "white",
+                boxShadow: "5px 5px 15px #424242",
+              }}
+            >
+              <CardContent>
+                <Typography
+                  style={{ opacity: "1" }}
+                  className={classes.cardTitle}
+                >
+                  Are you interested in receiving technical and industry related
+                  feedback from your favorite artists in the scene?
+                </Typography>
+                <br />
+                <Typography>
+                  <b>WavMovers</b> offers 1-on-1 lesson scheduling with
+                  professional artists to give you the tools you need to create.
+                  Our artists can cover a vast array of topics including: Track
+                  arrangement, DAW Workflow, Sound design, Mixing, Mastering,
+                  DJing, Music theory, Track feedback, Industry consulting, and
+                  Artistic identity guidance
+                </Typography>
+                <br />
+                <Typography>
+                  WavMovers 1-on-1 lessons will empower you to grow leaps and
+                  bounds in your musical journey through honing the skills
+                  needed to get you from the bedroom to the main stage
+                </Typography>
+                <br />
 
-								{showItem ? (
-									<CheckoutContainer />
-								) : (
-									<div>
-										<h3>$30/hr</h3>
-										<Button
-											className={classes.button}
-											onClick={() => setShowItem(true)}
-										>
-											Purchase a lesson!
-										</Button>
-									</div>
-								)}
-							</CardContent>
-						</Card>
-					</Grid>
-					{/* <Grid item style={{ width: "60%" }}>
+                {showItem ? (
+                  <CheckoutContainer />
+                ) : (
+                  <div>
+                    <h3>$30/hr</h3>
+                    <Button
+                      className={classes.button}
+                      onClick={() => setShowItem(true)}
+                    >
+                      Purchase a lesson!
+                    </Button>
+                  </div>
+                )}
+              </CardContent>
+            </Card>
+          </Grid>
+          {/* <Grid item style={{ width: "60%" }}>
             <InlineWidget
               styles={{
                 height: "650px",
@@ -103,10 +103,10 @@ function Lessons() {
               url="https://calendly.com/backleftmusic"
             />
           </Grid> */}
-				</Grid>
-			</div>
-		);
-	} else return <Login />;
+        </Grid>
+      </div>
+    );
+  } else return <Login />;
 }
 
 export default Lessons;
--- routes/stripe/stripe.js
+++ routes/stripe/stripe.js
@@ -1,56 +1,56 @@
-const router = require('express').Router();
-const mongoose = require('mongoose');
-const multer = require('multer');
-const Grid = require('gridfs-stream');
-const GridFsStorage = require('multer-gridfs-storage');
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const multer = require("multer");
+const Grid = require("gridfs-stream");
+const GridFsStorage = require("multer-gridfs-storage");
 const db = mongoose.connection;
-const User = require('../../models/User');
-const PackInfo = require('../../models/PackInfo');
-const crypto = require('crypto');
-const path = require('path');
-const cors = require('cors');
+const User = require("../../models/User");
+const PackInfo = require("../../models/PackInfo");
+const crypto = require("crypto");
+const path = require("path");
+const cors = require("cors");
 // needed for local payment testing, dotenv dev dependency also needed
 // require('dotenv').config();
-const stripe = require('stripe')(process.env.REACT_APP_STRIPE_KEY);
+const stripe = require("stripe")(process.env.REACT_APP_STRIPE_KEY);
 
 const calculateOrderAmount = () => {
-	return 3000;
+  return 3000;
 };
 
-router.post('/create-payment-intent', cors(), async (req, res) => {
-	console.log('payment being made!!');
+router.post("/create-payment-intent", cors(), async (req, res) => {
+  console.log("payment being made!!");
 
-	const { items, id } = req.body;
-	// Create a PaymentIntent with the order amount and currency
-	const paymentIntent = await stripe.paymentIntents.create({
-		amount: calculateOrderAmount(items),
-		currency: 'usd',
-		description: 'BackLeft Lesson',
-		payment_method: id,
-		confirm: true,
-	});
-	res.send({
-		message: 'Payment Successful',
-		success: true,
-	});
+  const { items, id } = req.body;
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "usd",
+    description: "BackLeft Lesson",
+    payment_method: id,
+    confirm: true,
+  });
+  res.send({
+    message: "Payment Successful",
+    success: true,
+  });
 });
 
-router.post('/create-payment-intent', cors(), async (req, res) => {
-	console.log('payment being made!!');
+router.post("/create-payment-intent", cors(), async (req, res) => {
+  console.log("payment being made!!");
 
-	const { items, id } = req.body;
-	// Create a PaymentIntent with the order amount and currency
-	const paymentIntent = await stripe.paymentIntents.create({
-		amount: calculateOrderAmount(items),
-		currency: 'usd',
-		description: 'BackLeft Lesson',
-		payment_method: id,
-		confirm: true,
-	});
-	res.send({
-		message: 'Payment Successful',
-		success: true,
-	});
+  const { items, id } = req.body;
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "usd",
+    description: "BackLeft Lesson",
+    payment_method: id,
+    confirm: true,
+  });
+  res.send({
+    message: "Payment Successful",
+    success: true,
+  });
 });
 
 module.exports = router;
